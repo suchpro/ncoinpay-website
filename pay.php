99df3b8cb8a7ff6ac9b6193bb1a2daab02e3f470
ncoincrypto <45012905+ncoincrypto@users.noreply.github.com>
2021-12-31 18:58:08 +0000

    add stuff

--- pay.php
+++ pay.php
@@ -1,5 +1,19 @@
 <html>
-    <title>hi</title>
+    <title>Buy now with NCoin Pay</title>
+    <link rel="stylesheet" href="style.css">
+    <div class="center">
+        <div class="paybox">
+            <p>Pay with NCoin</p>
+            <img src="" title="Payment QR"/>
+        </div>
+    </div>
 </html>
 
-<?php echo("hi")?>
+<?php
+    $merchantid = $_GET["merchantid"];
+    $productid = $_GET["productid"];
+    $providedorderid = $_GET["orderid"];
+    $npayid = $merchantid . $productid . $providedorderid;
+    $payamount = json_decode(file_get_contents("https://algoindexer.algoexplorerapi.io/v2/accounts/2J7RQGH7BLTE5JYSVPL5T57VIWB67SUOBFEFEJLAIIZNRVNJBF3TAMARAI/transactions"), true);
+    echo($payamount["current-round"])
+?>
