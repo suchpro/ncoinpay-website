2fbb305e0da2c61432609ca4c1ddbf534f507e6c
ncoincrypto <45012905+ncoincrypto@users.noreply.github.com>
2022-01-02 11:20:20 +0000

    commir

--- pay.php
+++ pay.php
@@ -10,6 +10,11 @@
     $productid = $_GET["productid"];
     $providedorderid = $_GET["orderid"];
     $npayid = $merchantid . $productid . $providedorderid;
-    $productprice = json_decode(file_get_contents("https://algoindexer.algoexplorerapi.io/v2/accounts/2J7RQGH7BLTE5JYSVPL5T57VIWB67SUOBFEFEJLAIIZNRVNJBF3TAMARAI/transactions"), true)["current-round"]*100000;
-    echo('<div class="center"><div class="paybox">    <p>Pay with NCoin</p>    <img src="' . 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' . 'algorand%3A%2F%2F4UU5LKME4OJUHHOVIK7S6KDVBGYCOXE5G43367M775OWIEK4SSRRG5VYSY%3Famount%3D'. $productprice .'%26asset%3D338543684%26xnote%3Dnpay' . $npayid . '&choe=UTF-8' . '" title="Payment QR"/> </div></div>');
+    $productpricedict = json_decode(file_get_contents("https://algoindexer.algoexplorerapi.io/v2/transactions/" . $productid), true);
+
+    if $productpricedict["sender"] == $merchantid{
+        $productprice = json_decode(base64_decode($productpricedict["note"]))["price-in-ncoin"]
+    }
+
+    echo('<div class="center"><div class="paybox">    <p>Pay with NCoin</p>    <img src="' . 'https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl=' . 'algorand%3A%2F%2F' . $merchantid . '%3Famount%3D'. $productprice .'%26asset%3D338543684%26xnote%3Dnpay' . $npayid . '&choe=UTF-8' . '" title="Payment QR"/> </div></div>');
 ?>
