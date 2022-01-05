49a56f8dd48c544f81454334bd1151c8b686e27d
ncoincrypto <45012905+ncoincrypto@users.noreply.github.com>
2022-01-05 16:45:42 +0000

    commit

--- pay.php
+++ pay.php
@@ -1,13 +1,13 @@
 <html>
-    <title>Buy now with NCoin Pay</title>
-
+    <title>NCoin Pay</title>
     <style>
         .lds-ring {
             display: inline-block;
             position: relative;
             width: 100px;
             height: 100px;
-            padding-top: 1000px;
+            padding-left: 50px;
+            padding-bottom: 180px;
         }
         .lds-ring div {
             box-sizing: border-box;
@@ -39,7 +39,7 @@
             }
         }
     </style>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </html>
 
 <?php
@@ -51,10 +51,49 @@
     $productpricedict = json_decode(file_get_contents("https://algoindexer.algoexplorerapi.io/v2/transactions/" . $productid), true);
         
     $productprice = null;
-    if ($productpricedict["sender"] == $merchantid and $productpricedict["payment-transaction"]["receiver"] == $merchantid)
+    if ($productpricedict["transaction"]["sender"] == $merchantid and $productpricedict["transaction"]["payment-transaction"]["receiver"] == $merchantid)
     {
-        $productprice = json_decode(base64_decode($productpricedict["note"]))["price-in-ncoin"];
+        $productprice = json_decode(base64_decode($productpricedict["transaction"]["note"]), true)["price-in-ncoin"];
     }
-    echo($productprice);
-    echo('<div class="center"><div class="paybox">    <p>Pay with NCoin</p>    <img src="' . 'https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl=' . 'algorand%3A%2F%2F' . $merchantid . '%3Famount%3D'. $productprice .'%26asset%3D338543684%26xnote%3Dnpay' . $npayid . '&choe=UTF-8' . '" title="Payment QR"/>     <div class="lds-ring"><div></div><div></div><div></div><div></div></div>  </div></div>');
+    echo('<div class="center"><div class="paybox">  <button id="myalgo" class="myalgo">1 Click with MyAlgo</button>  <p>Pay with NCoin</p> <p class="depositaddress">Send ' . $productprice . ' NCoin to ' . $merchantid . '</p>    <img id="qr" src=' . 'https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl=' . 'algorand%3A%2F%2F' . $merchantid . '%3Famount%3D'. $productprice .'%26asset%3D338543684%26xnote%3Dnpay' . $npayid . '&choe=UTF-8' . '" title="Payment QR"/>     <div class="lds-ring"><div></div><div></div><div></div><div></div></div>  </div></div>');
 ?>
+
+<script src="https://content.ncoincrypto.com/myalgo.min.js"></script>
+<script src="https://unpkg.com/algosdk@1.13.0-beta.2/dist/browser/algosdk.min.js" integrity="sha384-ArIfXzQ4ARpkRJIn6EKgtqbJaPXhEEvNoguSPToHMg2VNl2rNc6QuuOTyDX7Krps" crossorigin="anonymous"></script>
+
+<script>
+    async function asyncCall() {
+        
+        const myAlgoConnect = new MyAlgoConnect();
+        const accountsSharedByUser = await myAlgoConnect.connect();
+
+        const algodClient = new algosdk.Algodv2('', 'https://node.algoexplorerapi.io/', '');
+        const params = await algodClient.getTransactionParams().do();
+
+        let sender = "RABQ4PRRWJDCMHCCCZ24YTLTT44MI4KFZEMRWGFEYESDRBIKD6QFPNNGBQ";
+        let receiver = "5YOGUR4XWHBVD2TEMG77XULMSQSK6DTQ4HUEHJA5ILA4FSRSMMKZSRWBK4";
+        let note = undefined
+        let revocationTarget = undefined;
+        let closeRemainderTo = undefined;
+        //Amount of the asset to transfer
+        amount = <?php echo("$productprice")?>
+
+        // signing and sending "txn" will send "amount" assets from "sender" to "recipient"
+        const objtxn = {
+            ...params,
+            type: 'axfer',
+            from: accountsSharedByUser[0],
+            to: receiver,
+            assetIndex: 338543684,
+            amount: amount,
+            note: note
+        };  
+        const signedTxn = await myAlgoConnect.signTransaction(objtxn);
+        const response = await algodClient.sendRawTransaction(signedTxn.blob).do();
+    }
+
+    document.getElementById("myalgo").onclick = function()
+    {
+        asyncCall();
+    }
+</script>
